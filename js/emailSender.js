ae9e3b63badf37396fe9049dfba9c7a93651a022
John Ashley Torio <johnashley132002@gmail.com>
2025-09-01 11:24:54 +0800

    test

--- js/emailSender.js
+++ js/emailSender.js
@@ -17,7 +17,7 @@ contactForm.addEventListener('submit', async function (e) {
   submitEmailBtn.disabled = true;
 
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbwWXuwwx4', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbwWXuwwx4gFTROUoG-W6n9ikmGvHptcdNG1goAf3ukA829oAtQW10eOtx91tLElbynAvg/exec', {
       method: 'POST',
       mode: "no-cors",
       body: new FormData(contactForm) 
